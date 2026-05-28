d8a4cfac4010f02dcc1494490457d4a0afe54084
Niphony <nicoud1027@gmail.com>
2026-05-28 15:54:49 -0500

    Inicio de nodo AVL

---
+++ Nodo_AVL.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+
+struct Nodo{
+  int dato;
+  Nodo* izq;
+  Nodo* der;
+  int altura;  
+};
+
+class AVLnodo {
+
+
+
+public:
+int NodoAVL(int dato, int altura){
+
+  Nodo* nodo1 = new Nodo();
+  Nodo* nodo2 = new Nodo();
+  
+
+  return 0;
+
+}
+
+};
